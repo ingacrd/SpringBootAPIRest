fa87e0525e9e2c2bd8f5162bf735064fd8bed060
Carolina Ruiz <Carolina.Ruiz@FutureElectronics.com>
2025-07-09 15:14:03 -0400

    get list

---
+++ src/main/java/com/ingaru/springbootrest/Alien.java
@@ -0,0 +1,39 @@
+package com.ingaru.springbootrest;
+
+
+
+
+
+public class Alien {
+	private int Id;
+	private String name;
+	private int points;
+	public String getName() {
+		return name;
+	}
+	
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	@Override
+	public String toString() {
+		return "Alien [Id=" + Id + ", name=" + name + ", points=" + points + "]";
+	}
+	
+	
+}
